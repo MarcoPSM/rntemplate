ed37f56875fcfd9c8da95ee04f40818371969ed1
Marco <marco.martins@novageo.pt>
2020-10-28 15:04:04 +0000

    First commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class App extends React.Component {
+    render() {
+      return (
+        <View>
+        <Text>Hello World</Text>
+        </View>
+      )
+    }
+}
+
+export default App;
